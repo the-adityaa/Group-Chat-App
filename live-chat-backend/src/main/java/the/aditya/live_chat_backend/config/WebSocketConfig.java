3c80187a00ace98d7857c4682b4cfbcbc03c8cf5
Aditya <aadigu1212@gmail.com>
2026-08-02 23:06:17 +0530

    Update tunnel URL

--- live-chat-backend/src/main/java/the/aditya/live_chat_backend/config/WebSocketConfig.java
+++ live-chat-backend/src/main/java/the/aditya/live_chat_backend/config/WebSocketConfig.java
@@ -1,32 +1,4 @@
 package the.aditya.live_chat_backend.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-
-        config.enableSimpleBroker("/topic");
-        // /topic/messages
-
-        config.setApplicationDestinationPrefixes("/app");
-        // /app/chat
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat") // connection establish
-                .setAllowedOrigins(
-                        "http://localhost:5173",
-                        "https://group-chat-app-phi.vercel.app"
-                )
-                .withSockJS();
-    }
+public class WebSocketConfig {
 }
